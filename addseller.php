995d2383112561dd38528ea420872b68cc16c50f
Shailendra Mukati <shailendram9165@gmail.com>
2022-06-09 12:03:08 +0530

    sm1

--- addseller.php
+++ addseller.php
@@ -418,7 +418,7 @@
     </div>
   </div>
             <br><br>
-            <form action="validsellerregister.php" method="POST">
+            <form action="validadminseller.php" method="POST">
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group first">
@@ -531,18 +531,9 @@
                 </div>
               </div>
               
-              <div class="d-flex mb-5 mt-4 align-items-center" >
-                <div class="d-flex align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Creating an account means you're okay with our <a href="#">Terms and Conditions</a> and our <a href="#">Privacy Policy</a>.</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label>
-              </div>
-              </div>
              <input type="submit" class="btn btn-primary mt-3 wow zoomIn" value="Register"> 
-              <p><a href="sellerlogin.php">Already registered? Back to Login</a></p>
-            </form>
-                
+               </form>
+                <br><br>
                     </div>
 
                 </div>
